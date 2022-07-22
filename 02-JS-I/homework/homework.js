bae0f8730632b84650ad2bb25efa238695363245
luis <luis@LUIS>
2022-07-21 23:07:39 -0600

    Finalizando ejemplo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -239,14 +239,14 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   
-}if(letra.length > 1){
+if(letra.length > 1){
   return "Dato incorrecto"
 }
 if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
   return "Es vocal"
 }
 return "Dato incorrecto"
-
+}
 
 
 // No modificar nada debajo de esta línea
