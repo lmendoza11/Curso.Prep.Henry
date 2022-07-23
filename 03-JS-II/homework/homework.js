4c70350928b591a541f3ef48303692ec1e3e10ea
Admin <luis.mendoza11@gmail.com>
2022-07-23 15:58:16 -0400

    Update Homework JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -2,7 +2,7 @@
 
 function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
-  // Devuelve el número más grande
+  // Devuelve el número más grande.
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
   if(x > y) {
