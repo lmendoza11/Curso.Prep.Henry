59c6dc2ec50fd73dbd97a6a31d131d569d3aa15a
Admin <luis.mendoza11@gmail.com>
2022-07-23 16:18:02 -0400

    Update homework IV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -4,7 +4,7 @@ function crearGato(nombre, edad) {
   // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
   // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
-  // Devuelve el objeto
+  // Devuelve el objeto.
   // Tu código:
   var obj = {
     nombre: nombre,
